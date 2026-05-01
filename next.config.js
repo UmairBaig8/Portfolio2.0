01727559458f9212b40fea618c0bf2d4677d5c3c
Umair Baig <umairbaig808@gmail.com>
2026-05-01 17:13:52 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['example.com'], // Add your image domains
-  },
-  env: {
-    MY_SECRET: process.env.MY_SECRET, // Add your environment variables
-  },
-};
